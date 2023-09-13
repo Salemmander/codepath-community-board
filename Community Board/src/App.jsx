86e652f13d0ded313c2a7c9ee3379b9d2192abda
Salemmander <salemnassar5@gmail.com>
2023-09-13 13:47:57 -0500

    Created Restaurant Card Component

--- Community Board/src/App.jsx
+++ Community Board/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Restaurant from "./components/Restaurant";
 
 function App() {
-    return <div>This a is a test</div>;
+    return (
+        <div>
+            <h1 className='title'>Place actual name later</h1>
+            <Restaurant
+                image='https://www.fbgcdn.com/pictures/3fa18f3e-0eea-4f16-8352-1cf92ddca41b.jpg'
+                name='Hoki Poki'
+                cuisine='Hawaiian'
+                link='https://www.orderhokipoki.com/'
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ Community Board/src/components/Restaurant.jsx
@@ -0,0 +1,15 @@
+import "../App.css";
+const Restaurant = (props) => {
+    return (
+        <div className='restaurantCard'>
+            <img src={props.image} alt='' />
+            <h2>{props.name}</h2>
+            <h4>{props.cuisine}</h4>
+            <a href={props.link}>
+                <button className='viewMenu'>View Menu</button>
+            </a>
+        </div>
+    );
+};
+
+export default Restaurant;
